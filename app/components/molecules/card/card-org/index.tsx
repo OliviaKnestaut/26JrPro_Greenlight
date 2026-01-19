9f02fb844af30728b92c54e09955d9e5a4aefc02
hannah-dez <hd434@drexel.edu>
2026-01-19 11:47:43 -0500

    more org card updates

--- app/components/molecules/card/card-org/index.tsx
+++ app/components/molecules/card/card-org/index.tsx
@@ -8,12 +8,12 @@ export type CardOrgProps = React.ComponentProps<typeof Card> & {
 };
 
 const CardOrg: React.FC<CardOrgProps> = ({ children, title, avatarSrc, subtitle, ...rest }) => (
-    <Card className={styles.card} title= 'Drexel Women in Business' {...rest}>
-        <div className={styles.content}>
+    <Card className={styles.card} {...rest}>
         <div className={styles.header}>
         {avatarSrc && <Avatar src={avatarSrc} />}
-        {subtitle && <div className={styles.subtitle}></div>}
         </div>
+        <div className={styles.content}>
+        {subtitle && <div className={styles.subtitle}> DU Women in Business</div>}
         Drexel Women in Business is a joint student organization. Drexel Women in Business (DWIB) is a network of dynamic, like-minded women achieving their business goals through support, inclusion, inspiration, and mentoring. DWIB maintains a strong network of women in the business community by coordinating networking events, speaker series, workshops, and similar activities. These events are open to the entire Drexel University community in order to foster growth, relationships, and future opportunities. This organization emphasizes LeBow's ties to the alumni network and to the greater Philadelphia business community, and upholds LeBow's commitment to excellence. </div>
     </Card>
 );
